fb2121a7cb5b5a0e8cb4f48e720e4672bba07258
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-07-06 23:16:20 +0800

    更新 dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -87,7 +87,7 @@ def main():
     # 获取最新优选IP
     ip_addresses_str = get_cf_speed_test_ip()
     lines = ip_addresses_str.split()
-    ip_addresses = lines
+    ip_addresses = lines[0]
     dns_records = get_dns_records(CF_DNS_NAME)
     push_plus_content = []
     # 遍历 IP 地址列表

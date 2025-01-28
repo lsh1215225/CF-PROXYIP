f65eeac9cc09495ad865f7733dcb82144ae6b890
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-01-28 18:09:02 +0800

    更新 df_sorted.py

--- df_sorted.py
+++ df_sorted.py
@@ -10,7 +10,7 @@ df['峰值速度数值'] = df['峰值速度'].str.replace(' kB/s', '').astype(fl
 df_sorted = df.sort_values(by='峰值速度数值', ascending=False)
 
 # 添加双重筛选条件：国家为US且IP类型为隧道
-df_gj = df_sorted[(df_sorted['国家'] == 'US') & (df_sorted['IP类型'] == '隧道')]
+df_gj = df_sorted[(df_sorted['国家'] == 'SG') & (df_sorted['IP类型'] == '隧道')]
 
 # 将排序后的DataFrame保存回CSV文件
 df_gj.to_csv('latest_sorted.csv', index=False)
